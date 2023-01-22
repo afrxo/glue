476791b8f84c9be40ee305d63a8fa0cb68e76275
afrxo <davidyboakye1@outlook.com>
2023-01-22 17:15:36 +0100

    Renamed: Bundle -> Packages

--- build.lua
+++ build.lua
@@ -1,7 +1,7 @@
 -- build.lua / Build Glue
 
 local Place = remodel.readPlaceFile("Bundle.rbxl")
-local Bundle = Place.ReplicatedStorage.Bundle
+local Bundle = Place.ReplicatedStorage.Packages
 Bundle.Name = "Packages"
 Bundle.Glue.Packages:Destroy()
 remodel.writeModelFile(Bundle, "Glue.rbxm")
