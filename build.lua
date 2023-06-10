31bec00b5097de11c9ee441416fa375e109cc08f
afrxo <davidyboakye1@outlook.com>
2023-06-10 17:10:20 +0200

    Migrated build script

--- build.lua
+++
@@ -1,7 +0,0 @@
--- build.lua / Build Glue
-
-local Place = remodel.readPlaceFile("Bundle.rbxl")
-local Bundle = Place.ReplicatedStorage.Packages
-Bundle.Name = "Packages"
-Bundle.Glue.Packages:Destroy()
-remodel.writeModelFile(Bundle, "Glue.rbxm")
---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+project=bundle.project.json
+output=Glue.rbxm
+
+rojo build $project -o $output
